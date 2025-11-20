35bee38150aaf601698d046066860ae80fe153ac
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-11-20 14:46:29 -0600

    refactor: extract individual Card component

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,17 +2,21 @@ export default function Cards({ cardData }) {
   return (
     <div>
       {cardData.map((meme) => {
-        return (
-          <div
-            className="card"
-            style={{ width: "200px", height: "auto" }}
-            key={meme.id}
-          >
-            <img src={meme.url} alt={`Meme: ${meme.name}`} />
-            {meme.name}
-          </div>
-        );
+        return <Card meme={meme} />;
       })}
+    </div>
+  );
+}
+
+function Card({ meme }) {
+  return (
+    <div
+      className="card"
+      style={{ width: "200px", height: "auto" }}
+      key={meme.id}
+    >
+      <img src={meme.url} alt={`Meme: ${meme.name}`} />
+      <h2>{meme.name}</h2>
     </div>
   );
 }
