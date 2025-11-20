52d442b701aee30bb520ec185e213217b47e712a
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-11-20 16:32:32 -0600

    feat: shuffle cardData for random display order

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,9 +1,11 @@
 import "./Cards.css";
 
 export default function Cards({ cardData }) {
+  const shuffledCards = shuffle(cardData);
+
   return (
     <div className="card-grid">
-      {cardData.map((meme) => {
+      {shuffledCards.map((meme) => {
         return <Card meme={meme} />;
       })}
     </div>
@@ -26,4 +28,24 @@ function Card({ meme }) {
       {/* <h2 className="meme-card__title">{meme.name}</h2> */}
     </div>
   );
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
